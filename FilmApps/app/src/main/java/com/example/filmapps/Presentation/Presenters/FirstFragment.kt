d3dbd9fbf2b921df5f6f1452dbbba7ba1ba72e83
ilmal <ilmalyshev1000@mail.ru>
2022-07-12 14:09:50 +0400

    arch

--- FilmApps/app/src/main/java/com/example/filmapps/Presentation/Presenters/FirstFragment.kt
+++ FilmApps/app/src/main/java/com/example/filmapps/Presentation/Presenters/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.filmapps
+package com.example.filmapps.Presentation.Presenters
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.filmapps.R
 import com.example.filmapps.databinding.FragmentFirstBinding
 
 /**
--- FilmApps/app/src/main/java/com/example/filmapps/Presentation/Presenters/MainActivity.kt
+++ FilmApps/app/src/main/java/com/example/filmapps/Presentation/Presenters/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.filmapps
+package com.example.filmapps.Presentation.Presenters
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.filmapps.R
 import com.example.filmapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
